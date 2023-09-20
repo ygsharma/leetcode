77f49c4ba4ea1517ae4111df43e158a368bd8be7
Yogesh Sharma <yogesh.sharma.007321@gmail.com>
2023-09-20 17:07:54 -0400

    q33 added

---
+++ 33. Search in Rotated Sorted Array/33.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        
+        int s = 0, e = nums.length-1;
+
+        while(s <= e) {
+
+            int mid = s + (e-s)/2;
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] >= nums[s]) {
+                if(target >= nums[s] && target < nums[mid]) e = mid-1;
+                else s = mid+1;
+            }
+            else if(nums[mid] <= nums[e]){
+                if(target > nums[mid] && target <= nums[e]) s = mid+1;
+                else e = mid-1;
+            }
+        }
+        return -1;
+    }
+
+    // key point: when we divide the array one part will be sorted.
+}
