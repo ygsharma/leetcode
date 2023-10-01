3dfca6d0d3ba4719ac3b1747ad4ac8f1a4ce87f2
Yogesh Sharma <yogesh.sharma.007321@gmail.com>
2023-09-30 23:09:50 -0400

    lc m q39 added

---
+++ 39. Combination Sum/39.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        
+        List<Integer> li = new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(candidates, target, 0, li, ans);
+        return ans;
+    }
+
+    public void helper(int[] nums, int target, int idx, List<Integer> li, List<List<Integer>> ans) {
+
+        if(idx >= nums.length) {
+            if(target == 0) ans.add(new ArrayList<>(li));
+            return;
+        }
+        if(target < 0) return;
+
+        if(target == 0) {
+            ans.add(new ArrayList<>(li));
+            return;
+        }
+        
+        li.add(nums[idx]);
+        helper(nums, target-nums[idx], idx, li, ans);
+    
+        li.remove(li.size()-1);
+        helper(nums, target, idx+1, li, ans);
+    }
+}
